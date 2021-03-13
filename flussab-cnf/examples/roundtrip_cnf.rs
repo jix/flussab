bd554dd216e2011c9c5e587fcfb8e47873c63e11
Jannis Harder <me@jix.one>
2021-03-13 11:32:38 +0100

    CNF roundtrip example

---
+++ flussab-cnf/examples/roundtrip_cnf.rs
@@ -0,0 +1,30 @@
+use std::io::Write;
+
+use flussab::ByteWriter;
+
+use flussab_cnf::{cnf, ParseError};
+
+fn main() {
+    if let Err(err) = main_err() {
+        eprintln!("error: {}", err);
+    }
+}
+
+fn main_err() -> Result<(), ParseError> {
+    let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
+
+    let mut cnf_reader = cnf::Parser::<i32>::from_read(stdin.lock(), false)?;
+    let mut cnf_writer = ByteWriter::from_write(stdout.lock());
+
+    if let Some(header) = cnf_reader.header() {
+        cnf::write_header(&mut cnf_writer, header)?;
+    }
+
+    while let Some(lits) = cnf_reader.next_clause()? {
+        cnf::write_clause(&mut cnf_writer, lits)?;
+    }
+
+    cnf_writer.flush()?;
+    Ok(())
+}
